f0f61e4281d6f57ceb3d00e0ce63468487f64835
kechen666 <ncuchenke@gmail.com>
2024-10-15 17:09:15 +0800

    feat(hypergraph): create a class MLD_Hypergraph, input a detector error model and get a hypergraph

---
+++ google_qec_surface_code/MLD_Hypergraph.py
@@ -0,0 +1,94 @@
+import stim
+
+import logging
+# 设置 logging 配置
+# logging.basicConfig(level=logging.WARNING)
+logger = logging.getLogger(__name__)
+
+from typing import List, Union, Optional, Tuple, Set, Dict
+
+class MLD_Hypergraph:
+    def __init__(self, detector_error_model: stim.DetectorErrorModel):
+        self.detector_error_model = detector_error_model
+        self.nodes: List[str]
+        self.hyperedges: List[List[str]]
+        self.nodes, self.hyperedges = self.detector_error_model_to_hypergraph(detector_error_model)
+
+    def get_nodes(self) -> List[str]:
+        return self.nodes
+
+    def get_hyperedges(self) -> List[List[str]]:
+        return self.hyperedges
+
+    def detector_error_model_to_hypergraph(self, detector_error_model: stim.DetectorErrorModel)-> Tuple[List[str], List[List[str]]]:
+        """将错误检测模型转换为超图。
+
+        Args:
+            detector_error_model (stim.DetectorErrorModel): 错误检测模型
+
+        Returns:
+            Tuple[List[str], List[List[str]]]: 节点集合和超边集合
+        """
+        self.detector_number = detector_error_model.num_detectors
+        self.logical_observable_number = detector_error_model.num_observables
+        
+        nodes = self.detector_and_logical_observable_number_to_hypernodes(self.detector_number, self.logical_observable_number)
+        hyperedges = self.detector_error_model_to_hyperedge(detector_error_model)
+        return nodes, hyperedges
+    # 直接从MLD中移植过来的，后续调整两个类的使用
+
+    def detector_and_logical_observable_number_to_hypernodes(self, detector_number: int, logical_observable_number: int, have_logical_observable: bool = False) -> List[str]:
+        """获取超图节点集合。
+
+        Args:
+            detector_number (int): 错误检测模型中检测器的数量。
+            logical_observable_number (int): 错误检测模型中逻辑可观测量的数量。
+            have_logical_observable (bool, optional): 是否有逻辑可观测量。默认为False。
+            
+        Returns:
+            List[str]: 超图节点集合。例如["D0","D1","D2","L0","L1"]
+        """
+        detector_nodes = [f"D{i}" for i in range(detector_number)]
+        
+        logical_observable_nodes = [f"L{i}" for i in range(logical_observable_number)]
+        if have_logical_observable:
+            nodes = detector_nodes + logical_observable_nodes
+        else:
+            nodes = detector_nodes
+        
+        return nodes
+
+    def detector_error_model_to_hyperedge(self, detector_error_model: stim.DetectorErrorModel) -> List[List[str]]:
+        """将detector_error_model中的error事件转换为超图边。
+
+        Args:
+            detector_error_model (stim.DetectorErrorModel): 错误检测模型, 通过stim进行生成。
+
+        Returns:
+            List[List[str]]: 超图边集合。例如[["D0","D1","D2"],["D3","D4","D5"]]
+        """
+        hyperedges = []
+        for error in detector_error_model:
+            if error.type == "error":
+                error_even = error.targets_copy()
+                hyperedge = self.error_even_to_hyperedge(error_even)
+                hyperedges.append(hyperedge)
+        return hyperedges
+
+    def error_even_to_hyperedge(self, error_even: List[stim.DemTarget], have_logical_observable: bool = False) -> List[str]:
+        """从一个error事件中,转换为一条超图边hyperedge。即错误事件翻转的detector和logical_observable的index。
+
+        Args:
+            error_even (List[stim.DemTarget]): 错误事件中的detector或logical_observable对象
+            have_logical_observable (bool, optional): 是否有logical_observable作为超图节点. Defaults to False.
+            
+        Returns:
+            List[str]: 超图对应的边. 例如["D0","D1","D2"]
+        """
+        hyperedge = []
+        for flip_object in error_even:
+            if flip_object.is_relative_detector_id():
+                hyperedge.append(f"D{flip_object.val}")
+            elif have_logical_observable and flip_object.is_logical_observable_id():
+                hyperedge.append(f"L{flip_object.val}")
+        return hyperedge
